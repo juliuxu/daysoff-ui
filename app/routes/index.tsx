a89d119e0e1dc4b507d83b8b52a4a66a37d2bba1
Julian Jark <juliuxu@gmail.com>
2022-12-18 06:06:36 +0100

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,18 +2,25 @@ import { Form, useLoaderData, useSubmit } from "@remix-run/react";
 import { z } from "zod";
 
 import { cabinProperties, CabinProperty, fixDatesInline } from "~/domain";
-import { allowsDogs, Category } from "~/domain";
+import { Category } from "~/domain";
 
 import { CabinTable } from "~/components/cabin-table";
 import type { LoaderArgs } from "@remix-run/cloudflare";
 import { redirect } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { CachedDaysoffApi } from "~/service/daysoff/cf-cached-api";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+const dateRangeSchema = z.preprocess((arg) => {
+  if (typeof arg == "string") {
+    return arg.split("_").map((x) => new Date(x));
+  }
+}, z.tuple([z.date(), z.date()]));
 
 const formSchema = z.object({
   category: z.nativeEnum(Category),
   properties: z.array(z.nativeEnum(CabinProperty)).default([]),
+  dates: z.array(dateRangeSchema).default([]),
 });
 
 export const loader = async ({ context, request }: LoaderArgs) => {
@@ -23,6 +30,7 @@ export const loader = async ({ context, request }: LoaderArgs) => {
   const input = formSchema.parse({
     ...Object.fromEntries(searchParams),
     properties: searchParams.getAll("properties"),
+    dates: searchParams.getAll("dates"),
   });
 
   const cabins = await new CachedDaysoffApi(context, {}).fetchCabinsForCategory(
@@ -50,6 +58,7 @@ export default function Index() {
         <section>
           <Form
             onChange={(e) => {
+              console.log("got onChange", e);
               submit(e.currentTarget);
             }}
           >
@@ -86,11 +95,10 @@ export default function Index() {
                 </label>
               ))}
             </fieldset>
+            <DaterangeList name="dates" defaultValues={data.input.dates} />
           </Form>
         </section>
         Count: {cabins.length}
-        <br />
-        Count allow dogs: {cabins.filter((cabin) => allowsDogs(cabin)).length}
         <CabinTable cabins={cabins} />
         {/* {cabins.map((cabin) => (
           <CabinCard key={cabin.link} cabin={cabin} />
@@ -111,4 +119,108 @@ const CabinPropertyTitles: Record<CabinProperty, React.ReactNode> = {
   [CabinProperty.Sauna]: "Badstue 🧖",
   [CabinProperty.Hottub]: "Boblebad ♨️",
   [CabinProperty.Internet]: "Internett 🌐",
+};
+
+interface DaterangeListProps {
+  name: string;
+  defaultValues: [from: string, to: string][];
+}
+const DaterangeList = ({ name, defaultValues }: DaterangeListProps) => {
+  const fieldsetRef = useRef<HTMLFieldSetElement>(null);
+  const [selectedDates, setSelectedDates] = useState(() =>
+    defaultValues
+      .slice()
+      .sort((a, b) => (a < b ? -1 : 1))
+      .map((l) => l.map((d) => d.split("T")[0])),
+  );
+  const triggerOnChange = () => {
+    const e = new Event("change");
+    fieldsetRef.current?.dispatchEvent(e);
+    console.log("dispatch", fieldsetRef.current?.dispatchEvent);
+  };
+
+  const add = (v: [from: string, to: string]) => {
+    setSelectedDates((l) => [...l, v].slice().sort((a, b) => (a < b ? -1 : 1)));
+    setTimeout(() => triggerOnChange(), 100);
+  };
+  const remove = ([from, to]: [from: string, to: string]) => {
+    setSelectedDates((l) =>
+      l.filter(([xFrom, xTo]) => xFrom !== from && xTo !== to),
+    );
+    // setTimeout(() => triggerOnChange(), 100);
+  };
+
+  // Date range input
+  const toRef = useRef<HTMLInputElement>(null);
+  const fromRef = useRef<HTMLInputElement>(null);
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const addDateRange = () => {
+    if (!to || !from) return;
+    add([from, to]);
+    setFrom("");
+    setTo("");
+  };
+
+  useEffect(() => {
+    if (from && to) {
+      addDateRange();
+      fromRef.current?.blur();
+      toRef.current?.blur();
+    } else if (from) toRef.current?.focus();
+    else if (to) fromRef.current?.focus();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [from, to]);
+
+  return (
+    <>
+      <fieldset ref={fieldsetRef}>
+        <legend>
+          <strong>Dato</strong>
+        </legend>
+        <ul className="flex flex-wrap gap-1">
+          {selectedDates.map(([from, to]) => (
+            <li
+              key={`${from}_${to}`}
+              className="inline-block border p-0.5 text-sm"
+            >
+              <input type="hidden" name={name} value={`${from}_${to}`} />
+              {from} - {to}
+              <button onClick={() => remove([from, to])}>X</button>
+            </li>
+          ))}
+        </ul>
+      </fieldset>
+
+      <div>
+        <label>
+          Fra
+          <input
+            ref={fromRef}
+            type="date"
+            value={from}
+            max={to}
+            onChange={(e) => {
+              e.stopPropagation();
+              setFrom(e.currentTarget.value);
+            }}
+          />
+        </label>
+        <label>
+          Til
+          <input
+            ref={toRef}
+            type="date"
+            value={to}
+            min={from}
+            onChange={(e) => {
+              e.stopPropagation();
+              setTo(e.currentTarget.value);
+            }}
+          />
+        </label>
+        {/* <button onClick={addDateRange}>Legg til</button> */}
+      </div>
+    </>
+  );
 };
