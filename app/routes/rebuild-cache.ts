1af5662142bc5772c81026706a2128b412b13826
Julian Jark <juliuxu@gmail.com>
2022-12-18 19:06:00 +0100

    rebuild smarter

--- app/routes/rebuild-cache.ts
+++ app/routes/rebuild-cache.ts
@@ -1,10 +1,27 @@
 import type { LoaderArgs } from "@remix-run/cloudflare";
+import { redirect } from "@remix-run/cloudflare";
 import { json } from "react-router";
+import { z } from "zod";
 import { Category } from "~/domain";
 import { CachedDaysoffApi } from "~/service/daysoff/cf-cached-api";
 
-export const loader = async ({ context }: LoaderArgs) => {
-  const api = new CachedDaysoffApi(context, { maxAge: 60 * 3 });
+const formSchema = z.object({
+  category: z.nativeEnum(Category).optional(),
+});
+
+export const loader = async ({ context, request }: LoaderArgs) => {
+  const searchParams = new URL(request.url).searchParams;
+
+  const input = formSchema.parse({
+    ...Object.fromEntries(searchParams),
+  });
+
+  const api = new CachedDaysoffApi(context, { maxAge: 60 * 10 });
+  if (input.category) {
+    return json({
+      [input.category]: await api.fetchCabinsForCategory(input.category),
+    });
+  }
   return json({
     mountain: await api.fetchCabinsForCategory(Category.Mountain),
     ocean: await api.fetchCabinsForCategory(Category.Ocean),
